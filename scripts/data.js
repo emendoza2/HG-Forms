2423066077a0320b03e8f79f9bd2ad804e32224c
emendoza2 <elijahtanchimendoza@gmail.com>
2017-03-28 13:02:17 +0800

    Added philippine/us accreditation
    Buttons at start for accreditation - they work funky

--- scripts/data.js
+++ scripts/data.js
@@ -60,154 +60,155 @@ function createGradeSheet() {
   }
   return values;
 }
-var data = [{
-		label: "Please input your name",
-		required: true,
-		fail: /^((?!(^[a-z\-]+\ ?,\ ?[a-z\-]+(\ [a-z\-]+\.?)?$)).)*$/i,
-		messages: {
-			empty: "Please input your name",
-			error: "Invalid name provided - place your last name followed by a comma and your first name"
-		},
-		customPlaceholder: "Last name, First name, (Middle name or Middle initial - optional)"
-	},
-	{
-		label: "What is your child's name?",
-		required: true,
-		fail: /^((?!(^[a-z\-]+\ ?,\ ?[a-z\-]+(\ [a-z\-]+\.?)?$)).)*$/i,
-		messages: {
-			empty: "Please enter a name",
-			error: "Invalid name provided - place your last name followed by a comma and your first name"
-		},
-		customPlaceholder: "Last name, First name, (Middle name or Middle initial - optional)"
-	},
-	{
-		label: "Please input his or her birthdate",
-		required: true,
-		custom: "<div class='input-group date'>" +
-			"<input type='text' class='form-control' name='input3' value='03/13/2017' />" +
-			"<span class='input-group-addon'>" +
-			"<span class='glyphicon glyphicon-calendar'></span>" +
-			"</span>" +
-			"</div>"
-	},
-	{
-		label: "What is your student's level?",
-		required: true,
-		custom: "<select class='selectpicker' name='input4'>" +
-			"<option>Pre-K</option>" +
-			"<option>Kindergarden</option>" +
-			"<option>Grade 1</option>" +
-			"<option>Grade 2</option>" +
-			"<option>Grade 3</option>" +
-			"<option>Grade 4</option>" +
-			"<option>Grade 5</option>" +
-			"<option>Grade 6</option>" +
-			"<option>Grade 7</option>" +
-			"<option>Grade 8</option>" +
-			"<option>Grade 9</option>" +
-			"<option>Grade 10</option>" +
-			"<option>Grade 11</option>" +
-			"<option>Grade 12</option>" +
-			"</select>"
-	},
-	{
-		label: "What school year is your student in?",
-		required: true,
-		custom: "<input type='text' class='input-group form-control date' id='year-dp' name='input5' value=' 2016' /><br /><span id='sy'></span>"
-	},
-	{
-		label: "What quarter is your student in?",
-		required: true,
-		custom: '<label class="radio-inline"><input checked="checked" type="radio" name="input6" value="Q1" />Q1</label>' +
-			'<label class="radio-inline"><input type="radio" name="input6" value="Q2" />Q2</label>' +
-			'<label class="radio-inline"><input type="radio" name="input6" value="Q3" />Q3</label>' +
-			'<label class="radio-inline"><input type="radio" name="input6" value="Q4" />Q4</label>'
-	},
-	{
-		inputs: createGradeSheet("Language","Reading"),/*[
-      {
-    		label: "Language",
-    		required: true,
-    		fail: /$a/,
-    		messages: {
-    			error: "",
-    			empty: "Please input the name of your language curriculum"
-    		},
-        customPlaceholder: "The name of your language curriculum"
-    	},
-      {
-    		label: "Grades",
-    		required: true,
-    		fail: /^((?!^(\d+)\/(\d+)$|(^[1-9][0-9]?(\.[0-9]+)?|^100(\.0+)?)(%)$).)*$/,
-    		messages: {
-    			error: "Please input a percentage or a fraction",
-    			empty: "Please input a grade"
-    		},
-        customPlaceholder: "E.G. 9/10 or 90%"
-    	},
-    	{
-    		label: "Reading",
-    		required: true,
-    		fail: /$a/,
-    		messages: {
-    			error: "",
-    			empty: "Please input the name of your reading curriculum"
-    		},
-        customPlaceholder: "The name of your reading curriculum"
-    	},
-      {
-    		label: "Grades",
-    		required: true,
-    		fail: /^((?!^(\d+)\/(\d+)$|(^[1-9][0-9]?(\.[0-9]+)?|^100(\.0+)?)(%)$).)*$/,
-    		messages: {
-    			error: "Please input a percentage or a fraction",
-    			empty: "Please input a grade"
-    		},
-        customPlaceholder: ""
-    	},
-    ],*/
-    contentText: "<h2>English Materials</h2>"
-	},
-  {
-    inputs: createGradeSheet("Filipino"),
-      /*{
-        label: "What is your material for Filipino?",
-        required: true,
-        fail: /$a/,
-        messages: {
-          error: "",
-          empty: "Please input the name of your Filipino curriculum"
-        }
-      },
-      {
-    		label: "Grades",
-    		required: true,
-    		fail: /^((?!^(\d+)\/(\d+)$|(^[1-9][0-9]?(\.[0-9]+)?|^100(\.0+)?)(%)$).)*$/,
-    		messages: {
-    			error: "Please input a percentage or a fraction",
-    			empty: "Please input a grade"
-    		},
-        customPlaceholder: "E.G. 9/10 or 90%"
-    	}
-      createGradeSheet("Filipino")
-    ],*/
-
-  },
-	{
-		inputs: createGradeSheet("Araling Palipunan"),
-	},
-	{
-    inputs: createGradeSheet("World History"),
-
-	},
-	{
-    inputs: createGradeSheet("Bible")
-	},
-	{
-		inputs: createGradeSheet("Character")
-	},
-	{
-		inputs: createGradeSheet("Mother Tongue"),
-    contentText: "<small>For grades 1-3 only<small>"
-	}
-];
+var philippine = {
+  inputs: [
+    {
+  		label: "Please input your name",
+  		required: true,
+  		fail: /^((?!(^[a-z\-]+\ ?,\ ?[a-z\-]+(\ [a-z\-]+\.?)?$)).)*$/i,
+  		messages: {
+  			empty: "Please input your name",
+  			error: "Invalid name provided - place your last name followed by a comma and your first name"
+  		},
+  		customPlaceholder: "Last name, First name, (Middle name or Middle initial - optional)"
+  	},
+  	{
+  		label: "What is your child's name?",
+  		required: true,
+  		fail: /^((?!(^[a-z\-]+\ ?,\ ?[a-z\-]+(\ [a-z\-]+\.?)?$)).)*$/i,
+  		messages: {
+  			empty: "Please enter a name",
+  			error: "Invalid name provided - place your last name followed by a comma and your first name"
+  		},
+  		customPlaceholder: "Last name, First name, (Middle name or Middle initial - optional)"
+  	},
+  	{
+  		label: "Please input his or her birthdate",
+  		required: true,
+  		custom: "<div class='input-group date'>" +
+  			"<input type='text' class='form-control' name='input3' value='03/13/2017' />" +
+  			"<span class='input-group-addon'>" +
+  			"<span class='glyphicon glyphicon-calendar'></span>" +
+  			"</span>" +
+  			"</div>"
+  	},
+  	{
+  		label: "What is your student's level?",
+  		required: true,
+  		custom: "<select class='selectpicker' name='input4'>" +
+  			"<option>Pre-K</option>" +
+  			"<option>Kindergarden</option>" +
+  			"<option>Grade 1</option>" +
+  			"<option>Grade 2</option>" +
+  			"<option>Grade 3</option>" +
+  			"<option>Grade 4</option>" +
+  			"<option>Grade 5</option>" +
+  			"<option>Grade 6</option>" +
+  			"<option>Grade 7</option>" +
+  			"<option>Grade 8</option>" +
+  			"<option>Grade 9</option>" +
+  			"<option>Grade 10</option>" +
+  			"<option>Grade 11</option>" +
+  			"<option>Grade 12</option>" +
+  			"</select>"
+  	},
+  	{
+  		label: "What school year is your student in?",
+  		required: true,
+  		custom: "<input type='text' class='input-group form-control date' id='year-dp' name='input5' value=' 2016' /><br /><span id='sy'></span>"
+  	},
+  	{
+  		label: "What quarter is your student in?",
+  		required: true,
+  		custom: '<label class="radio-inline"><input checked="checked" type="radio" name="input6" value="Q1" />Q1</label>' +
+  			'<label class="radio-inline"><input type="radio" name="input6" value="Q2" />Q2</label>' +
+  			'<label class="radio-inline"><input type="radio" name="input6" value="Q3" />Q3</label>' +
+  			'<label class="radio-inline"><input type="radio" name="input6" value="Q4" />Q4</label>'
+  	},
+  	{
+  		inputs: createGradeSheet("Language","Reading"),/*[
+        {
+      		label: "Language",
+      		required: true,
+      		fail: /$a/,
+      		messages: {
+      			error: "",
+      			empty: "Please input the name of your language curriculum"
+      		},
+          customPlaceholder: "The name of your language curriculum"
+      	},
+        {
+      		label: "Grades",
+      		required: true,
+      		fail: /^((?!^(\d+)\/(\d+)$|(^[1-9][0-9]?(\.[0-9]+)?|^100(\.0+)?)(%)$).)*$/,
+      		messages: {
+      			error: "Please input a percentage or a fraction",
+      			empty: "Please input a grade"
+      		},
+          customPlaceholder: "E.G. 9/10 or 90%"
+      	},
+      	{
+      		label: "Reading",
+      		required: true,
+      		fail: /$a/,
+      		messages: {
+      			error: "",
+      			empty: "Please input the name of your reading curriculum"
+      		},
+          customPlaceholder: "The name of your reading curriculum"
+      	},
+        {
+      		label: "Grades",
+      		required: true,
+      		fail: /^((?!^(\d+)\/(\d+)$|(^[1-9][0-9]?(\.[0-9]+)?|^100(\.0+)?)(%)$).)*$/,
+      		messages: {
+      			error: "Please input a percentage or a fraction",
+      			empty: "Please input a grade"
+      		},
+          customPlaceholder: ""
+      	},
+      ],*/
+      contentText: "<h2>English Materials</h2>"
+  	},
+    {
+      inputs: createGradeSheet("Filipino"),
+    },
+  	{
+  		inputs: createGradeSheet("Araling Palipunan"),
+  	},
+  	{
+      inputs: createGradeSheet("World History"),
+  	},
+  	{
+      inputs: createGradeSheet("Bible")
+  	},
+  	{
+  		inputs: createGradeSheet("Character")
+  	},
+  	{
+  		inputs: createGradeSheet("Mother Tongue"),
+      contentText: "<small>For grades 1-3 only<small>"
+  	}
+  ],
+  execute: function(){
+    var currentDate = (new Date());
+    $("#year-dp").datepicker( {
+        format: " yyyy",
+        viewMode: "years",
+        minViewMode: "years",
+        endDate: currentDate,
+        startDate: currentDate.dateAdd("year",-2)
+    })
+    .on("changeDate",function(e){
+      var year = e.date.getFullYear();
+      $("#sy").html(year+" - "+(year+1));
+    });
+    $("#year-dp").val(" " + currentDate.getFullYear());
+    $("#year-dp").datepicker('update');
+    $(".date").datepicker( {
+      endDate: currentDate,
+      assumeNearbyYear: true
+    });
+    $('.selectpicker').selectpicker();
+  }
+};
--- scripts/forms.js
+++ scripts/forms.js
@@ -20,7 +20,7 @@ function createStep(opt) {
   var labels = [];
   var $label;
   var $input;
-  //var $input;
+
   if (opt.custom !== undefined) {
     $label = $("<label></label>")
       .html(fallback(opt.inputs[0].labelContent));
@@ -97,3 +97,291 @@ function createStep(opt) {
   // $step.appendTo(opt.parentForm); - this should be done outside of this. Otherwise, what is the point of return?
   return $step;
 }
+console.log("breakpoint1");
+Date.prototype.dateAdd = function(size,value) {
+  var newDate = new Date(this);
+  value = parseInt(value);
+  var incr = 0;
+  switch (size) {
+      case 'day':
+          incr = value * 24;
+          newDate.dateAdd('hour',incr);
+          return newDate;
+      case 'hour':
+          incr = value * 60;
+          newDate.dateAdd('minute',incr);
+          return newDate;
+      case 'week':
+          incr = value * 7;
+          newDate.dateAdd('day',incr);
+          return newDate;
+      case 'minute':
+          incr = value * 60;
+          newDate.dateAdd('second',incr);
+          return newDate;
+      case 'second':
+          incr = value * 1000;
+          newDate.dateAdd('millisecond',incr);
+          return newDate;
+      case 'month':
+          value = value + newDate.getUTCMonth();
+          if (value/12>0) {
+              newDate.dateAdd('year',value/12);
+              value = value % 12;
+          }
+          newDate.setUTCMonth(value);
+          return newDate;
+      case 'millisecond':
+          newDate.setTime(newDate.getTime() + value);
+          return newDate;
+      case 'year':
+          newDate.setFullYear(newDate.getUTCFullYear()+value);
+          return newDate;
+      default:
+          throw new Error('Invalid date increment passed');
+          //break;
+  }
+}; // for later
+// Style Guide: //
+/*
+* When creating an element, prefix it's var name with $
+* Define it with only the minimum valid html tag required for that element - e.g. $("<a></a>")
+* All text content in the element must be added using .html()
+* All attributes are defined using .attr or .addClass
+* All children must be appended to the parent with append or appendTo
+*/
+
+// Define everything here
+
+// Constants
+var fadeOutTime = 300;
+var fadeInTime = 500;
+
+
+// Vars
+
+/* PARAMETERS FOR STEP():
+* step (step index)
+* noSteps (number of steps)
+* buttonCallback (duh)
+* labelContent (optional)
+* (does not do anything anymore) parentForm
+* content (optional)
+* visible (optional, but reccomended)
+* inputPlaceholder (optional)
+* buttonText (also optional)
+*/
+
+
+// Utility functions
+  function finish() {
+    for (var i = 0; i < steps.length; i ++) {
+      steps[i].fadeOut(fadeOutTime);
+    }
+  }
+
+  function refreshActive(dir) {
+
+    function /* local! */ createAlert(text) { // DRY
+      var $alert = $("<div></div>")
+        .addClass("alert")
+        .addClass("alert-danger")
+        .addClass("alert-dismissable")
+        .attr("role","alert")
+        .append(
+          $("<button></button>")
+            .addClass("close")
+            .attr("type","button")
+            .attr("data-dismiss","alert")
+            .attr("aria-label","Close")
+            .html($("<span></span>").attr("aria-hidden","true").html("&times;"))
+        )
+        .append(text);
+        return $alert;
+    }
+
+    function /* local */ nextStep(){ //DRY DRY DRY!!!!
+        canPressButton = false; // ugh..
+        steps[currentStep].addClass("ignore");
+        steps[currentStep].fadeOut(fadeOutTime,function() {
+            steps[currentStep].removeClass("ignore");
+
+            canPressButton = true; // yuck..
+
+            currentStep += 1; // incrememt currentStep (yech)
+
+            function /* (local) */ fadeIn() {
+              steps[currentStep].fadeIn(fadeInTime);
+            }
+
+            if (currentStep < noSteps) { // make sure we don't get a nil step
+              fadeIn();
+            } else {
+              currentStep = steps.length - 1;
+              //complete the form
+              fadeIn();
+            }
+        }); // increment step. once that is done, fade in the next step
+    }
+
+    if ( dir === undefined || dir === 1 ) {
+
+      var allOK = true;
+      for (var i = 0; i < data[currentStep].inputs.length; i++) {
+      var currentData = data[currentStep].inputs[i];
+      var failRegex = currentData.fail;
+      if (failRegex !== undefined) {
+
+        var inputValue = $("#input"+(currentStep+1)+"-"+i).val(); // more DRY
+        var $alert;
+        if (inputValue.match(failRegex)) {
+          $alert = createAlert(currentData.messages.error); // create alert with error message
+          allOK = false;
+        } else if(currentData.required === true && (inputValue === "" || inputValue.match(/^\s+$/))) { // kill nasty empty input fields (if they are required)
+          console.log(currentData.messages.empty);
+          $alert = createAlert(currentData.messages.empty); // create alert with empty message
+          allOK = false;
+        }
+        steps[currentStep].append("<br />").append($alert === undefined ? "" : $alert); // display alert or nothing if there was no error
+      }
+    }
+    if (allOK) { // need this, otherwise multiple steps will appear out of nowhere!
+      nextStep();
+    }
+    } else {
+      canPressButton = false; // ugh..
+      steps[currentStep].addClass("ignore");
+      steps[currentStep].fadeOut(fadeOutTime,function() {
+        steps[currentStep].removeClass("ignore");
+
+        canPressButton = true; // yuck..
+
+        currentStep -= 1; // incrememt currentStep (yech)
+
+        function /* (local) */ fadeIn() {
+          steps[currentStep].fadeIn(fadeInTime);
+        }
+
+        if (currentStep < noSteps) { // make sure we don't get a nil step
+          fadeIn();
+        } else {
+          currentStep = steps.length - 1;
+          //complete the form
+          fadeIn();
+        }
+      }); // increment step. once that is done, fade in the next step
+    }
+  }
+  // end
+
+
+
+  // Main code (time to *do* stuff)
+  function defaultCallback(e) {
+      e.preventDefault();
+      if (canPressButton) {
+        console.log("refreshed");
+        refreshActive();
+      } else {
+        console.log(canPressButton);
+      }
+  }
+  function goback(e) {
+    e.preventDefault();
+    if (canPressButton) {
+      refreshActive(-1);
+    }
+  }
+
+
+  function createInput(datas, inputs) {
+    return {
+      inputPlaceholder: datas.customPlaceholder,
+      customInputType: datas.customInputType,
+      inputRequired: datas.inputRequired,
+      labelContent: datas.label,
+      fail: datas.fail,
+      messages: datas.messages,
+      inline: datas.inline
+    };
+  }
+
+  function startForm(data) {
+    var $form, steps, canPressButton, currentStep, noSteps, $finalForm, $button, $head; // define all variables so they can be accessed through the console (will be changed later for security)
+
+    $form = $("<form></form>").attr("id","mainForm"); // the form container
+
+    steps = []; // the array that will hold all the steps
+    canPressButton = true; // this is sooo ugly.. I wish I could remove it
+    currentStep = 0; // I don't know if I need this either
+    noSteps = data.inputs.length; // the number of steps
+    // final area with "submit" button
+    $finalForm = $("<div></div>")
+      .addClass("form-group")
+      .addClass("col-md-6");
+    $button = $("<input />")
+      .addClass("btn")
+      .addClass("btn-success")
+      .attr("type","submit")
+      .html("Finish")
+      .click(finish);
+    $head = $("<h4></h4>")
+      .html("Thank you for using this form");
+    $finalForm
+      .append($("<br />"))
+      .append($head)
+      .append($("<br />"))
+      .append($button)
+      .fadeOut(0);
+
+    // Note to self: js arrays start at 0!
+    for (var i = 0; i < noSteps; i ++) {
+      var inputs = [];
+      var currentData = data.inputs[i];
+      if (typeof currentData.inputs == 'object') { // create multiple
+        console.log(currentData.inputs);
+        for (var j = 0; j < currentData.inputs.length; j++) {
+          currentData = currentData.inputs[j];
+          console.log(currentData.customPlaceholder);
+          inputs.push(createInput(currentData));
+        }
+      } else {
+        inputs.push(createInput(currentData));
+        currentData.inputs = inputs;
+      }
+      steps[i] = createStep({
+        step: i+1, // put this so that steps are shown as index in array + 1
+        noSteps: noSteps,
+        buttonText: "&gt;&gt;",
+        buttonCallback: defaultCallback,
+        previousText: i > 0 ? "&lt;&lt;" : undefined,
+        previousCallback: goback,// no need for check here; previous is defined by previousText
+        content: currentData.contentText === undefined ? "<br />" : data.inputs[i].contentText + "<br />",
+        //labelContent: data.inputs[i].label,
+        custom: currentData.custom,
+        inputs: inputs
+      });
+      steps[i].addClass("ignore");
+    }
+
+    steps.push($finalForm);
+
+    for (i = 0; i < steps.length; i ++) {
+      var $el = steps[i];
+      $el.appendTo($form);
+    }
+    $form.appendTo(".container");
+    steps[0].fadeIn(500);
+
+    $form.submit(function(e) { // at last, we intercept the form and gloriously console.log the output!!!
+      e.preventDefault();
+      var formSubmission = $(this).serializeArray();
+      console.log(formSubmission); // for me
+      alert(JSON.stringify(formSubmission)); //for mom
+    });
+
+    /* Rendering stuff */
+    data.execute();
+  }
+
+  console.log(startForm);
